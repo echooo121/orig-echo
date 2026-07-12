9bec8da6b7a6bcc4c4ab80b3cb14243e751a2ef9
NozasJ <johnbenedictsazonedu@gmail.com>
2026-07-12 21:38:20 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Echo System</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <nav class="navbar">
+                <a href="index.php" class="nav__logo">
+                    <img src="assets/echo-logo.svg"
+                        alt="Echo System Logo"
+                        class="nav__logo-img">
+                </a>
+                <a href="about.php"
+                            class="btn btn-secondary">
+                            About us
+                </a>
+            </nav>
+        </div>
+    </header>
