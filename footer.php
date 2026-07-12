54e303a008e5a4b736627198e49c0019aed4b78a
NozasJ <johnbenedictsazonedu@gmail.com>
2026-07-12 21:38:18 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+        <div class="container">
+            <p>
+                © 2026 Echo System. All rights reserved.
+            </p>
+            <p>
+                Disclaimer:
+                This website is for educational purposes only.
+                It was developed as part of an academic project.
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
