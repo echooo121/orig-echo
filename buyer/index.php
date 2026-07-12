c0f05cda9667352f9d387ff95112229500c784e7
echooo121 <echosystem21@gmail.com>
2026-07-12 17:52:55 +0800

    Add new files

---
+++ buyer/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Echo System</title>
+</head>
+
+<body>
+        <header class="header">
+
+        <div class="container">
+
+            <nav class="navbar">
+
+                <!-- Logo -->
+
+                <a href="index.php" class="nav__logo">
+
+                    <img src="../echo-logo.svg"
+                        alt="Echo System Logo"
+                        class="nav__logo-img">
+
+                </a>
+
+                <!-- Right Button -->
+
+                <div class="nav__buttons">
+
+                    <a href="admin/login.php"
+                        class="btn btn-primary">
+
+                        Admin Login
+
+                    </a>
+
+                </div>
+
+            </nav>
+
+        </div>
+
+    </header>
+
+    <main>
+
+        <section class="hero">
+
+            <div class="container hero-container">
+
+                <!-- Left -->
+
+                <div class="hero-content">
+
+                    <span class="hero-tag">
+
+                        Everyday Carry Reimagined
+
+                    </span>
+
+                    <h1>
+
+                        Pack Smarter.
+                        <br>
+                        Travel Seamlessly.
+
+                    </h1>
+
+                    <p>
+
+                        Thoughtfully designed bags that connect,
+                        organize, and move with you.
+
+                        Whether you're commuting to work,
+                        attending classes,
+                        or travelling across the country,
+                        Echo System helps you stay organized
+                        with premium modular bags built
+                        for modern lifestyles.
+
+                    </p>
+
+                    <div class="hero-buttons">
+
+                        <a href="buyer/register.php"
+                            class="btn btn-primary">
+
+                            Join for Free
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <!-- Right -->
+
+                <div class="hero-image">
+
+                    <img src="assets/img/landing-page-bag.png"
+                        alt="Echo System Travel Bag">
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+
+    <footer class="footer">
+
+        <div class="container">
+
+            <p>
+
+                Disclaimer:
+                This website is for educational purposes only.
+                It was developed as part of an academic project.
+
+            </p>
+
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
