080667490d7992b2df03ad0dbdc4132a775fd086
NozasJ <johnbenedictsazonedu@gmail.com>
2026-07-13 02:00:31 +0800

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,9 @@
     <section class="section register_section">
     <div class="container">
         <div class="register_card">
-
+            <?php if (!empty($message)): ?>
+                <p class="form_message <?php echo $type; ?>"><?php echo htmlspecialchars($message); ?></p>
+            <?php endif; ?>
             <div class="register_header">
                 <h2>Register</h2>
                 <p>Create your account to easily manage your orders, shipping details, and gear rewards.</p>
@@ -13,28 +15,28 @@
 
                 <div class="form_row">
                     <div class="input">
-                        <i class="ri-user-line"></i>
+                       
                         <input type="text" name="first_name" placeholder="First Name" value="<?php echo isset($first_name) ? htmlspecialchars($first_name) : ''; ?>" required>
                     </div>
                     <div class="input">
-                        <i class="ri-user-line"></i>
+                        
                         <input type="text" name="last_name" placeholder="Last Name" value="<?php echo isset($last_name) ? htmlspecialchars($last_name) : ''; ?>" required>
                     </div>
                 </div>
 
                 <div class="input">
-                    <i class="ri-mail-line"></i>
+                    
                     <input type="email" name="email" placeholder="Email" value="<?php echo isset($email) ? htmlspecialchars($email) : ''; ?>" required>
                 </div>
 
                 <div class="input">
-                    <i class="ri-map-pin-line"></i>
+                    
                     <input type="text" name="address" placeholder="Full Address" value="<?php echo isset($address) ? htmlspecialchars($address) : ''; ?>" required>
                 </div>
 
                 <div class="input">
-                    <i class="ri-phone-line"></i>
-                    <input type="text" name="contact" placeholder="Contact no." value="<?php echo isset($contact) ? htmlspecialchars($contact) : ''; ?>" required>
+                   
+                    <input type="text" name="contact" maxlength="10" pattern="[0-9]{10}" placeholder="Contact no." value="<?php echo isset($contact) ? htmlspecialchars($contact) : ''; ?>" required>
                 </div>
 
                 <div class="form_row">
