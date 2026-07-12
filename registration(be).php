df46af188280c3140c3469f4e10fc6015269df98
NozasJ <johnbenedictsazonedu@gmail.com>
2026-07-13 02:00:33 +0800

    Update registration(be).php

--- registration(be).php
+++ registration(be).php
@@ -3,7 +3,7 @@ require('db(be).php');
 $message = "";
 $type = "";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
     $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -12,30 +12,33 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = mysqli_real_escape_string($conn, $_POST['password']);
     $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
 
-    if ($password !== $confirm_password) {
-        $message = "Passwords do not match.";
-        $type = "danger";
-    } else {
-        $userID = "2026".rand(1000, 9999);
-        while (true) {
-            $checkQuery = "SELECT * FROM users WHERE user_id = '$userID'";
-            $checkResult = mysqli_query($conn, $checkQuery);
-            if (mysqli_num_rows($checkResult) == 0) {
-                break;
-            }
-            $userID = "2026".rand(1000, 9999); 
-        }
-        $userType = "Buyer";
-        $sql = "INSERT INTO users (user_id, first_name, last_name, email, address, contact, password, user_type) VALUES ('$userID', '$first_name', '$last_name', '$email', '$address', $contact, '$password', '$userType')";
-
-        if (mysqli_query($conn, $sql)) {
-            $message = "Registration completed successfully.";
-            $type = "success";
-        } else {
-            $message = "Registration error: " . mysqli_error($conn);
+        if ($password !== $confirm_password) {
+            $message = "Passwords do not match.";
             $type = "danger";
+        } 
+        else {
+            $userID = "2026".rand(1000, 9999);
+            while (true){
+                $checkQuery = "SELECT * FROM users WHERE user_id = '$userID'";
+                $checkResult = mysqli_query($conn, $checkQuery);
+                if (mysqli_num_rows($checkResult) == 0) {
+                    break;
+                }
+                $userID = "2026".rand(1000, 9999); 
+            }
+            $userType = "Buyer";
+            $sql = "INSERT INTO users (user_id, first_name, last_name, email, address, contact, password, user_type) VALUES ('$userID', '$first_name', '$last_name', '$email', '$address', '$contact', '$password', '$userType')";
+
+            if (mysqli_query($conn, $sql)){
+                $message = "Registration completed successfully.";
+                $type = "success";
+                unset($first_name, $last_name, $email, $address, $contact);
+            } 
+            else{
+                $message = "Registration error: " . mysqli_error($conn);
+                $type = "danger";
+            }
         }
     }
-}
 
 ?>
