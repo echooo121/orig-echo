cd09a1a4d5d37ed1dc2d5ac68a11cffc83bd2c69
NozasJ <johnbenedictsazonedu@gmail.com>
2026-07-14 06:40:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,32 @@
 <?php require 'header.php'; ?>
-    <main>
-        <section class="frontindex">
-            <div class="container frontindex-container">
-                <div class="frontindex-content">
+    <section class="frontindex">
+        <div class="container frontindex-container">
+            <div class="frontindex-content">
+                <h1>
+                    Pack Smarter.
+                    <br>
+                    Travel Seamlessly.
+                </h1>
+                <p>
+                    Thoughtfully designed bags that connect,
+                    organize, and move with you.
                     
-                    <h1>
-                        Pack Smarter.
-                        <br>
-                        Travel Seamlessly.
-                    </h1>
-                    <p>
-                        Thoughtfully designed bags that connect,
-                        organize, and move with you.
-
-
-                        Whether you're commuting to work,
-                        attending classes,
-                        or travelling across the country,
-                        Echo System helps you stay organized
-                        with premium modular bags built
-                        for modern lifestyles.
-                    </p>
-                    <div class="frontindex-buttons">
-                        <a href="login.php" class="btn btn-primary">
-                            Join for Free
-                        </a>
-                    </div>
-                </div>
-                <div class="frontindex-image">
-                    <img src="assets/img/landing-page-bag.png" alt="Echo System Travel Bag">
+                    Whether you're commuting to work,
+                    attending classes,
+                    or travelling across the country,
+                    Echo System helps you stay organized
+                    with premium modular bags built
+                    for modern lifestyles.
+                </p>
+                <div class="frontindex-buttons">
+                    <a href="login.php" class="btn btn-primary">
+                        Join for Free
+                    </a>
                 </div>
             </div>
-        </section>
-    </main>
-    <?php require 'footer.php'; ?>
+            <div class="frontindex-image">
+                <img src="assets/img/landing-page-bag.png" alt="Echo System Travel Bag">
+            </div>
+        </div>
+    </section>
+<?php require 'footer.php'; ?>
