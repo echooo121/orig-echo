5b6108afd43016aa3708ac30da71bd6f303d2eeb
NozasJ <johnbenedictsazonedu@gmail.com>
2026-07-14 22:49:55 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
+    session_unset();
     session_destroy();
+
+    setcookie('email', '', time() - 3600);
+    setcookie('password', '', time() - 3600);
+
     header('Location: login.php');
-    exit;
+    die();
 ?>
