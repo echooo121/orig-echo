375853bb6c2182723dbe1def94666ccd3cee5a50
NozasJ <johnbenedictsazonedu@gmail.com>
2026-07-15 04:55:59 +0800

    Update registration(be).php

--- registration(be).php
+++ registration(be).php
@@ -1,14 +1,16 @@
 <?php
 require('db(be).php');
+require('mail.php');
 $message = "";
 $type = "";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    
     $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
     $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $address = mysqli_real_escape_string($conn, $_POST['address']);
-    $contact = "+63".mysqli_real_escape_string($conn, $_POST['contact']);
+    $contact = mysqli_real_escape_string($conn, $_POST['contact']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
     $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
 
@@ -16,7 +18,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $message = "Passwords do not match.";
         $type = "danger";
     }
+    elseif(!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $email)) {
+        $message = "Please enter a valid email address.";
+        $type = "danger";
+    }
+    elseif (!preg_match('/^(0?9\d{9})$/', $contact)) {
+        $message = "Please enter a valid PH mobile number (e.g. 9171234567).";
+        $type = "danger";
+    }
     else{
+        //replace leading 0 to +63 z
+        $contact_digits = preg_replace('/^0/', '', $contact);
+        $contact = "+63" . $contact_digits;
+
         $userID = "2026".rand(1000, 9999);
         while (true){
             $checkQuery = "SELECT * FROM users WHERE user_id = '$userID'";
@@ -27,14 +41,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $userID = "2026".rand(1000, 9999); 
         }
         $userType = "Buyer";
-        $sql = "INSERT INTO users (user_id, first_name, last_name, email, address, contact, password, user_type) VALUES ('$userID', '$first_name', '$last_name', '$email', '$address', '$contact', '$password', '$userType')";
-
-        if (mysqli_query($conn, $sql)){
-            $message = "Registration completed successfully.";
+        $token = bin2hex(random_bytes(32));
+        $sql = "INSERT INTO users (user_id, first_name, last_name, email, address, contact, password, user_type, token, is_confirmed) VALUES ('$userID', '$first_name', '$last_name', '$email', '$address', '$contact', '$password', '$userType', '$token', '0')";
+        if(mysqli_query($conn, $sql)) {
+        // SEND EMAIL
+        try {
+            send_confirmation_email($email, $first_name . " " . $last_name, $token);
+            $message = "Registered Successfully! Please confirm email address to log in.";
             $type = "success";
-            unset($first_name, $last_name, $email, $address, $contact);
+        } catch (Exception $e) {
+            $message = "Registration completed, but confirmation email was not sent: " . $e->getMessage();
+            $type = "danger";
         }
-        else{
+        } else {
             $message = "Registration error: " . mysqli_error($conn);
             $type = "danger";
         }
